51a2bdbda561519fa7bc313bc10e96079346c3a9
Oliwier965 <olikucz@vp.pl>
2021-01-01 17:18:56 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+let hamburger = document.querySelector('.hamburger');
+let nav = document.querySelector('.nav');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    nav.classList.toggle('open');
+});
